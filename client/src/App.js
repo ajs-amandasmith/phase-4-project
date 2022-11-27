d1d376fbac91895f717f1976cee5b0c78e9b64d5
Amanda Smith <ajs.amandasmith@gmail.com>
2022-11-27 16:24:04 -0600

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +12,19 @@ function App() {
       .then((data) => setCount(data.count));
   }, [])
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/testing">
+            <h1>Test Route</h1>
+          </Route>
+          <Route path="/">
+            <h1>Page Count: {count}</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+    
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
    # route to test your configuration
    get '/hello', to: 'application#hello_world'
 
+   get '*patch',
+        to: 'fallback#index',
+        constraints: ->(req) { !req.xhr? && req.format.html? }
 end

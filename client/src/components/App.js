412d75b9da05d973ea94a69478bead21bb894978
Amanda Smith <ajs.amandasmith@gmail.com>
2022-11-28 21:11:38 -0600

    NavBar styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 // import logo from '../logo.svg';
 import '../App.css';
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 
@@ -18,11 +18,11 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/testing">
-            <h1>Test Route</h1>
+            {/* <h1>Test Route</h1> */}
           </Route>
           <Route path="/">
             
-            <h1>Page Count: {count}</h1>
+            {/* <h1>Page Count: {count}</h1> */}
           </Route>
         </Switch>
       </div>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
   return (
     <div>
-      <p>Hello</p>
+      <nav class="nav nav-tabs flex-row">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Fanart</a>
+        <a class="nav-link" href="#">Artists</a>
+        <a class="nav-link ms-auto" href="#">Sign In</a>
+      </nav>
     </div>
   )
 }

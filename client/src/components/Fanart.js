73ac003f74bd58fb8481dcb47b5a254f2df222d6
Amanda Smith <ajs.amandasmith@gmail.com>
2022-12-26 16:45:22 -0600

    Fanart components added

---
+++ client/src/components/Fanart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Fanart() {
+  return (
+    <div>
+      Fanart
+    </div>
+  )
+}
+
+export default Fanart;
---
+++ client/src/components/FanartList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FanartList() {
+  return(
+    <div>
+      Listed Fanart
+    </div>
+  )
+}
+
+export default FanartList;
